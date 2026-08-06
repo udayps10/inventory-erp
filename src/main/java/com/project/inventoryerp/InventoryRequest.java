30e2516472b19ab8a5bd0e658b4c7b8acbbc24d5
udayps10 <uday2006is@gmail.com>
2026-08-07 00:15:12 +0530

    Update WarehouseRepository

---
+++ src/main/java/com/project/inventoryerp/InventoryRequest.java
@@ -0,0 +1,14 @@
+package com.project.inventoryerp;
+
+public class InventoryRequest {
+    private Long productId;
+    private Long warehouseId;
+    private Integer quantity;
+
+    public Long getProductId() { return productId; }
+    public void setProductId(Long productId) { this.productId = productId; }
+    public Long getWarehouseId() { return warehouseId; }
+    public void setWarehouseId(Long warehouseId) { this.warehouseId = warehouseId; }
+    public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+}
