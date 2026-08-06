aae2616eebd645298d9f6b3b32f5733056ab7976
udayps10 <uday2006is@gmail.com>
2026-08-07 00:11:08 +0530

    Update Category

--- src/main/java/com/project/inventoryerp/Category.java
+++ src/main/java/com/project/inventoryerp/Category.java
@@ -3,18 +3,24 @@ package com.project.inventoryerp;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "categories")
+@Table(name = "categories", uniqueConstraints = @UniqueConstraint(columnNames = {"business_id", "name"}))
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "business_id")
+    private Business business;
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    public Business getBusiness() { return business; }
+    public void setBusiness(Business business) { this.business = business; }
 }
