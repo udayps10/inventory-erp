c5cf2b5f11b462d6188b7f14fa1e3b5416fced8e
udayps10 <uday2006is@gmail.com>
2026-08-07 00:11:11 +0530

    Update InvoiceRepository

--- src/main/java/com/project/inventoryerp/InvoiceRepository.java
+++ src/main/java/com/project/inventoryerp/InvoiceRepository.java
@@ -1,4 +1,6 @@
 package com.project.inventoryerp;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+    long countByBusiness_Id(Long businessId);
 }
