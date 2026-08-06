007966575c917b93cc241a24ac5d2d65b14cb0de
udayps10 <uday2006is@gmail.com>
2026-08-07 00:11:13 +0530

    Update SecurityConfig

--- src/main/java/com/project/inventoryerp/SecurityConfig.java
+++ src/main/java/com/project/inventoryerp/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.project.inventoryerp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
