8f423752edcfda63896e97a35c1f6dd7183d55e7
udayps10 <uday2006is@gmail.com>
2026-08-07 00:11:09 +0530

    Update GlobalExceptionHandler

--- src/main/java/com/project/inventoryerp/GlobalExceptionHandler.java
+++ src/main/java/com/project/inventoryerp/GlobalExceptionHandler.java
@@ -2,14 +2,21 @@ package com.project.inventoryerp;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Map<String, String>> handleAccessDenied(AccessDeniedException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "You do not have permission to perform this action");
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+    }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
