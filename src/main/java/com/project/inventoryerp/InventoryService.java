210c5db585d3bee63c87c3e60055661d32d057e4
udayps10 <uday2006is@gmail.com>
2026-08-07 00:11:10 +0530

    Update InventoryService

--- src/main/java/com/project/inventoryerp/InventoryService.java
+++ src/main/java/com/project/inventoryerp/InventoryService.java
@@ -2,19 +2,17 @@ package com.project.inventoryerp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
 public class InventoryService {
 
-    @Autowired
-    private InventoryRepository inventoryRepository;
+    @Autowired private InventoryRepository inventoryRepository;
+    @Autowired private StockMovementRepository stockMovementRepository;
 
-    @Autowired
-    private StockMovementRepository stockMovementRepository;
-
+    @Transactional
     public StockMovement applyMovement(Product product, Warehouse warehouse, Integer quantityChange, String reason, String referenceId) {
-
         Inventory inventory = findInventory(product, warehouse);
 
         if (inventory == null) {
