58c03b986c95860d7e885c58ce450bb434d5fa52
udayps10 <uday2006is@gmail.com>
2026-08-07 00:11:11 +0530

    Update ProductRepository

--- src/main/java/com/project/inventoryerp/ProductRepository.java
+++ src/main/java/com/project/inventoryerp/ProductRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
     Optional<Product> findByBarcode(String barcode);
+
+    Optional<Product> findByIdAndBusiness(Long id, Business business);
 }
